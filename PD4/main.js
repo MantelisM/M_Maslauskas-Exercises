ec27bdfdd3db5a58a0f419965444ad21bebccece
Mantas Maslauskas <mantasmaslauskas2003@gmail.com>
2023-10-18 20:55:55 +0300

    PD4 solutions

---
+++ PD4/main.js
@@ -0,0 +1,16 @@
+// 1.
+let petName;
+let petAge;
+// 2.
+petName = "Bobik";
+petAge = 11;
+// 3.
+console.log("Pet's name: " + petName);
+console.log("Pet's age: " + petAge);
+// 4.
+petName = "Tuzik";
+petAge = 6;
+// 5.
+const buildingAddress = "Kalvariju 159.";
+// 6.
+console.log("Building Address: " + buildingAddress);
