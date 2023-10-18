24dff31f1bdec7f50c2932a1e4550fd0c96f9e06
Mantas Maslauskas <mantasmaslauskas2003@gmail.com>
2023-10-18 22:26:14 +0300

    PD7 solutions

--- PD7/JS/main.js
+++ PD7/JS/main.js
@@ -5,16 +5,16 @@ console.log(numberOne + numberTwo); // 30
 console.log(numberOne - numberTwo); // -10
 console.log(numberOne * numberTwo); // 200
 console.log(numberOne / numberTwo); // 0.5
-console.log(numberOne % numberTwo); // 10 (liekana po dalybos)
-console.log(numberOne ** numberTwo); // 10000000000000000000000000 (10 pakelta 20 laipsniu)
+console.log(numberOne % numberTwo); // 10 
+console.log(numberOne ** numberTwo); // 10000000000000000000000000 
 
-console.log(numberOne++); // 10 (naudojamas įrašymui, tada padidinamas)
-console.log(++numberOne); // 12 (pirma padidinamas, tada naudojamas)
+console.log(numberOne++); // 10 
+console.log(++numberOne); // 12 
 
-console.log(numberOne--); // 12 (naudojamas įrašymui, tada sumažinamas)
-console.log(--numberOne); // 10 (pirma sumažinamas, tada naudojamas)
+console.log(numberOne--); // 12 
+console.log(--numberOne); // 10 
 
-console.log(--numberOne + numberTwo++); // 29 (9 + 20)
-console.log(numberOne-- - --numberTwo + numberTwo++); 
-console.log(numberOne++ / numberTwo++); 
-console.log(numberOne++ * --numberTwo); 
+console.log(--numberOne + numberTwo++); // 29 
+console.log(numberOne-- - --numberTwo + numberTwo++); // 9 
+console.log(numberOne++ / numberTwo++); // 0.38095238095238093 
+console.log(numberOne++ * --numberTwo); // 189
